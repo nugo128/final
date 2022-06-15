00234d5153dba33913e1767acc7fe6a9f596cf8e
“nugo” <nugzari.rostiashvili.1@btu.edu.ge>
2022-06-15 20:26:35 +0400

    commit

---
+++ app .js
@@ -0,0 +1,335 @@
+const data = [
+  {
+    monday: [
+      {
+        id: 1,
+        start: 10,
+        end: 20,
+        title: "ურჩხული და ლამაზმანი",
+        cover:
+          "https://tkt-static.lemon.do/img/5161dbdb-0050-48f4-b7f2-4c21c8a7ba36.jpeg",
+      },
+      {
+        id: 2,
+        start: 10,
+        end: 20,
+        title: "მერე რა რომ, სველია სველი იასამანი",
+        cover:
+          "https://tkt-static.lemon.do/img/0f40afe7-396d-4158-8526-0b56b0c6b255.jpeg",
+      },
+      {
+        id: 3,
+        start: 10,
+        end: 20,
+        title: "შეყრა",
+        cover:
+          "https://tkt-static.lemon.do/img/68eaefd8-2a61-48bc-a0ce-7261aab14bc8.jpeg",
+      },
+    ],
+    tuesday: [
+      {
+        id: 4,
+        start: 10,
+        end: 20,
+        title: "მე, ბებია, ბაბუა და ჭინკები",
+        cover:
+          "https://tkt-static.lemon.do/img/410c4b0e-d63a-46c3-bb74-d3a03f753853.jpeg",
+      },
+      {
+        id: 5,
+        start: 10,
+        end: 20,
+        title: "ყვავილები ელჯერნონისთვის",
+        cover:
+          "https://tkt-static.lemon.do/img/053b9bdf-604e-4080-9b31-07d56e830f6d.jpeg",
+      },
+      {
+        id: 6,
+        start: 10,
+        end: 20,
+        title: "ფრთხილად ადამსები!",
+        cover:
+          "https://tkt-static.lemon.do/img/1db992f5-1d32-4dd7-a6a0-3bc668910b21.jpeg",
+      },
+    ],
+    wednesday: [
+      {
+        id: 7,
+        start: 10,
+        end: 20,
+        title: "სხვების ცხოვრება",
+        cover:
+          "https://tkt-static.lemon.do/img/01596ec6-7ef8-41dc-84a6-3adb84545d60.jpeg",
+      },
+      {
+        id: 8,
+        start: 10,
+        end: 20,
+        title: "რამონა",
+        cover:
+          "https://tkt-static.lemon.do/img/52247154-7cb1-495f-979c-013e2730e59c.jpeg",
+      },
+      {
+        id: 9,
+        start: 10,
+        end: 20,
+        title: "კომედიანტები",
+        cover:
+          "https://tkt-static.lemon.do/img/c0e68ee5-b119-476e-81f1-1bb33589e776.jpeg",
+      },
+    ],
+    thursday: [
+      {
+        id: 10,
+        start: 10,
+        end: 20,
+        title: "ჩემი გაზაფხულის შემოდგომა",
+        cover:
+          "https://tkt-static.lemon.do/img/94b6b77e-ca60-4b1a-b24a-3eb26092403d.jpeg",
+      },
+      {
+        id: 11,
+        start: 10,
+        end: 20,
+        title: "ანუ მე შენ მიყვარხარ",
+        cover:
+          "https://tkt-static.lemon.do/img/c19054cd-fa85-4924-9895-0ff6597dbfb5.jpeg",
+      },
+      {
+        id: 12,
+        start: 10,
+        end: 20,
+        title: "მოხუცი და ზღვა",
+        cover:
+          "https://tkt-static.lemon.do/img/2b4be8eb-c6be-4ef7-b4fb-c3933a45f123.jpeg",
+      },
+    ],
+    friday: [
+      {
+        id: 13,
+        start: 10,
+        end: 20,
+        title: "ყველაზე მაგარი გოგოები",
+        cover:
+          "https://tkt-static.lemon.do/img/a81df611-4dbd-4a91-8002-83abaeadd65d.jpeg",
+      },
+      {
+        id: 14,
+        start: 10,
+        end: 20,
+        title: "ალადინი",
+        cover:
+          "https://tkt-static.lemon.do/img/0d97441c-8656-49cf-bc49-f0c951a9e39b.jpeg",
+      },
+      {
+        id: 15,
+        start: 10,
+        end: 20,
+        title: "ურჩხული და ლამაზმანი",
+        cover:
+          "https://tkt-static.lemon.do/img/5161dbdb-0050-48f4-b7f2-4c21c8a7ba36.jpeg",
+      },
+    ],
+    saturday: [
+      {
+        id: 16,
+        start: 12,
+        end: 22,
+        title: "წითელქუდა",
+        cover:
+          "https://tkt-static.lemon.do/img/9a0ced6e-329b-4b6b-951e-1748c8621ee2.jpeg",
+      },
+      {
+        id: 17,
+        start: 12,
+        end: 22,
+        title: "გაყრა",
+        cover:
+          "https://tkt-static.lemon.do/img/b6e3f2c1-9cce-4fd4-8d42-4e71ffafb1a1.jpeg",
+      },
+      {
+        id: 18,
+        start: 12,
+        end: 22,
+        title: "ზღაპარი იგი მათრობს და მხიბლავს",
+        cover:
+          "https://tkt-static.lemon.do/img/f35816df-43b2-43ce-a844-3c89f82c439f.jpeg",
+      },
+    ],
+    sunday: [
+      {
+        id: 19,
+        start: 12,
+        end: 22,
+        title: "ცილინდრი",
+        cover:
+          "https://tkt-static.lemon.do/img/82c2e9e4-4ef7-4fe5-a8c9-008425babdf9.jpeg",
+      },
+      {
+        id: 20,
+        start: 12,
+        end: 22,
+        title: "შვლის ნუკრის ნაამბობი",
+        cover:
+          "https://tkt-static.lemon.do/img/f7ea467f-b4b4-42f8-9a0a-9fd0e9f7618f.jpeg",
+      },
+      {
+        id: 21,
+        start: 12,
+        end: 22,
+        title: "წითელი ფეხსაცმელები",
+        cover:
+          "https://tkt-static.lemon.do/img/228702ef-7486-4160-8b3e-d1dd60e50993.jpeg",
+      },
+    ],
+  },
+];
+const img1 = document.querySelector(".img1");
+const img2 = document.querySelector(".img2");
+const img3 = document.querySelector(".img3");
+const title1 = document.querySelector(".title1");
+const title2 = document.querySelector(".title2");
+const title3 = document.querySelector(".title3");
+const time1 = document.querySelectorAll(".time1");
+const time2 = document.querySelectorAll(".time2");
+const time3 = document.querySelectorAll(".time3");
+const time4 = document.querySelectorAll(".time4");
+const time5 = document.querySelectorAll(".time5");
+const allItems = document.getElementById("allitems");
+const timeSelector1 = document.getElementById("selecttime1");
+const timeSelector2 = document.getElementById("selecttime2");
+const timeSelector3 = document.getElementById("selecttime3");
+const btn1 = document.querySelector(".btn-buy1");
+const btn2 = document.querySelector(".btn-buy2");
+const btn3 = document.querySelector(".btn-buy3");
+const selectedDate = document.getElementById("selectdate");
+
+const displayItems = function (day) {
+  img1.src = data[0][day][0].cover;
+  img2.src = data[0][day][1].cover;
+  img3.src = data[0][day][2].cover;
+  title1.textContent = data[0][day][0].title;
+  title2.textContent = data[0][day][1].title;
+  title3.textContent = data[0][day][2].title;
+
+  for (let i = data[0][day][0].start; i < data[0][day][0].end; i += 2) {
+    if (i === 10) {
+      for (let k = 0; k < 3; k++) {
+        time1[k].textContent = `${i}:00-${i + 2}:00`;
+        time1[k].value = `${i}:00-${i + 2}:00`;
+      }
+    }
+    if (i === 12) {
+      for (let k = 0; k < 3; k++) {
+        if (day === "saturday" || day === "sunday") {
+          time1[k].textContent = `${i}:00-${i + 2}:00`;
+          time1[k].value = `${i}:00-${i + 2}:00`;
+        } else {
+          time2[k].textContent = `${i}:00-${i + 2}:00`;
+          time2[k].value = `${i}:00-${i + 2}:00`;
+        }
+      }
+    }
+    if (i === 14) {
+      for (let k = 0; k < 3; k++) {
+        if (day === "saturday" || day === "sunday") {
+          time2[k].textContent = `${i}:00-${i + 2}:00`;
+          time2[k].value = `${i}:00-${i + 2}:00`;
+        } else {
+          time3[k].textContent = `${i}:00-${i + 2}:00`;
+          time3[k].value = `${i}:00-${i + 2}:00`;
+        }
+      }
+    }
+    if (i === 16) {
+      for (let k = 0; k < 3; k++) {
+        if (day === "saturday" || day === "sunday") {
+          time3[k].textContent = `${i}:00-${i + 2}:00`;
+          time3[k].value = `${i}:00-${i + 2}:00`;
+        } else {
+          time4[k].textContent = `${i}:00-${i + 2}:00`;
+          time4[k].value = `${i}:00-${i + 2}:00`;
+        }
+      }
+    }
+    if (i === 18) {
+      for (let k = 0; k < 3; k++) {
+        if (day === "saturday" || day === "sunday") {
+          time4[k].textContent = `${i}:00-${i + 2}:00`;
+          time4[k].value = `${i}:00-${i + 2}:00`;
+        } else {
+          time5[k].textContent = `${i}:00-${i + 2}:00`;
+          time5[k].value = `${i}:00-${i + 2}:00`;
+        }
+      }
+    }
+    if (i === 20) {
+      for (let k = 0; k < 3; k++) {
+        time5[k].textContent = `${i}:00-${i + 2}:00`;
+        time5[k].value = `${i}:00-${i + 2}:00`;
+      }
+    }
+  }
+};
+let booked = [];
+
+selectedDate.addEventListener("change", () => {
+  displayItems(selectedDate.value);
+});
+let timesArray1 = [time1[0], time2[0], time3[0], time4[0], time5[0]];
+btn1.addEventListener("click", () => {
+  console.log(timeSelector1.value, img1.src, title1.textContent);
+  for (let i = 0; i < timesArray1.length; i++) {
+    if (timeSelector1.value === timesArray1[i].value) {
+      timesArray1[i].style.color = "red";
+      booked.push({
+        weekDay: selectedDate.value,
+        time: timeSelector1.value,
+        title: title1.textContent,
+        cover: img1.src,
+      });
+      console.log(booked);
+    }
+  }
+});
+let timesArray2 = [time1[1], time2[1], time3[1], time4[1], time5[1]];
+btn2.addEventListener("click", () => {
+  console.log(timeSelector2.value, img2.src, title2.textContent);
+  for (let i = 0; i < timesArray2.length; i++) {
+    if (timeSelector2.value === timesArray2[i].value) {
+      timesArray2[i].style.color = "red";
+      booked.push({
+        weekDay: selectedDate.value,
+        time: timeSelector2.value,
+        title: title2.textContent,
+        cover: img2.src,
+      });
+      console.log(booked);
+    }
+  }
+});
+let timesArray3 = [time1[2], time2[2], time3[2], time4[2], time5[2]];
+
+btn3.addEventListener("click", () => {
+  console.log(timeSelector3.value, img3.src, title3.textContent);
+  for (let i = 0; i < timesArray3.length; i++) {
+    if (timeSelector3.value === timesArray3[i].value) {
+      timesArray3[i].style.color = "red";
+      booked.push({
+        weekDay: selectedDate.value,
+        time: timeSelector3.value,
+        title: title3.textContent,
+        cover: img3.src,
+      });
+      console.log(booked);
+    }
+  }
+});
+
+const checkTimes = function () {
+  console.log("a");
+};
+
+selectedDate.addEventListener("change", () => {
+  displayItems(selectedDate.value);
+});
